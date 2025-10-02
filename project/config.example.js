b112788315054133efdaa0eb014d33f5e083a0f1
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-10-02 09:23:59 -0400

    foi?

--- project/config.example.js
+++
@@ -1,7 +0,0 @@
-
-const LOGIN_CREDENTIALS = {
-    username: 'SEU_USUARIO_AQUI',
-    password: 'SUA_SENHA_AQUI'
-};
-
-export { LOGIN_CREDENTIALS };
---
+++ project/config.js
@@ -0,0 +1,7 @@
+
+const LOGIN_CREDENTIALS = {
+    username: atob('VW5pdmFnQHJlcHJlc2VudGFudGVz'),
+    password: atob('VW5pdmFnQDIwMjU=') 
+};
+
+export { LOGIN_CREDENTIALS };
