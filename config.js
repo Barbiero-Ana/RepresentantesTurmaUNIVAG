3641286fae6d3cf7d9e990e42f6c3ba0685483f6
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-10-02 09:17:32 -0400

    vamos ver

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuração das credenciais de login
-// IMPORTANTE: Em produção, considere usar um backend para validação de credenciais
-const LOGIN_CREDENTIALS = {
-    username: 'Univag@representantes',
-    password: 'Univag@2025'
-};
-
-export { LOGIN_CREDENTIALS };
