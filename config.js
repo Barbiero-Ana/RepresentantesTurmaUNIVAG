7103ac673d43bb5eb7a1d8bc7070c745179c5852
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-10-02 09:27:40 -0400

    Funciona pelo amor de Deus

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const LOGIN_CREDENTIALS = {
-    username: atob('VW5pdmFnQHJlcHJlc2VudGFudGVz'),
+    username: atob('VW5pdmFnQHJlcHJlc2VudGFudGVz'), 
     password: atob('VW5pdmFnQDIwMjU=') 
 };
 
